bc7a3457e554e5e6ed85b2dfc9ab935c11038ec4
Beertoni <gustavohenriquebertoni@hotmail.com>
2021-09-28 13:26:06 -0300

    adicionando todos os assets e deixando front-end estático

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return view('site.home.index');
